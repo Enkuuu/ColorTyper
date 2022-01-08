dabdbb8584327dde8b9f8bd1cbc3eb938c01c3d1
Enku <95542651+Enkuuu@users.noreply.github.com>
2022-01-08 09:56:12 -0800

    shhhh

---
+++ script.js
@@ -0,0 +1,53 @@
+let wordsContainer = document.querySelector('#words');
+let YouWinText = document.querySelector('#YouWin');
+
+let words = [
+    {word: "red", color:"blue"},
+    {word: "blue", color:"orange"},
+    {word: "orange", color:"black"},
+    {word: "blue", color:"green"},
+    {word: "purple", color:"red"},
+    {word: "green", color:"orange"},
+    {word: "black", color:"purple"}, 
+]
+
+let wordsElement = []
+
+function showWords(){
+    words.forEach(function(word){
+        let newSpan = document.createElement('span');
+        newSpan.innerHTML = word.word;
+        newSpan.style.color = word.color;
+        wordsContainer.append(newSpan);
+        wordsElement.push(newSpan);
+    })
+}
+
+let wordsCounter = 0
+let startTime;
+let hasStarted = false;
+
+function checkColor(that){
+    let input = that.value;
+    that.value = '';
+
+    if (input.toLowerCase() == 'start'&& hasStarted==false){
+        hasStarted = true;
+        startTime = new Date();
+        showWords();
+    }
+
+    if(hasStarted){
+        if(input.toLowerCase() == words[wordsCounter].color){
+            wordsElement[wordsCounter].style.color = 'lightgray'
+            wordsCounter++
+
+            if(wordsCounter >= words.length){
+                let time = (new Date()) - startTime;
+                time /= 1000;
+                YouWinText.style.display= 'block';
+                YouWinText.innerHTML += " It took you " + time +'seconds';
+            }
+        }
+    }
+}
